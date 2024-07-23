1ba5833da946d25da48695c1d58b36084446ef4f
Rohan <rohan178.rd@gmail.com>
2024-07-23 09:01:55 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import API_list from './API_list';
+function App() {
+
+  return (
+    <>
+      <h1>API fetching practice</h1>
+      <API_list />      
+    </>
+  )
+}
+
+export default App
